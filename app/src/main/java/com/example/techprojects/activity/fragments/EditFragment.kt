1a39339a981a88c3dfa95642521e61382dd1f1a8
NISHKARSH SAXENA <94742536+nishkarsh800@users.noreply.github.com>
2023-08-10 15:24:59 +0530

    Added the fragments and made the navgraph

---
+++ app/src/main/java/com/example/techprojects/activity/fragments/EditFragment.kt
@@ -0,0 +1,23 @@
+package com.example.techprojects.activity.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.techprojects.R
+
+class EditFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit, container, false)
+    }
+}
