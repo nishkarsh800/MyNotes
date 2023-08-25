db63ee74572b482f14953bd56daafa8f5bf61d18
NISHKARSH SAXENA <94742536+nishkarsh800@users.noreply.github.com>
2023-08-25 13:19:38 +0530

    Showing the list items on main fragment

--- app/src/main/java/com/example/techprojects/activity/fragments/MainFragment.kt
+++ app/src/main/java/com/example/techprojects/activity/fragments/MainFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
 import com.example.techprojects.R
+import com.example.techprojects.activity.adapters.MyAdapter
+import com.example.techprojects.activity.viewmodel.NoteBookViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainFragment : Fragment() {
 
     lateinit var navController: NavController
+
+    var adapter = MyAdapter()
+
+    lateinit var viewModels:NoteBookViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,18 +30,31 @@ class MainFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+
+        viewModels=ViewModelProvider(this)[NoteBookViewModel::class.java]
+
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         navController = Navigation.findNavController(view)
 
-        val buttton = view.findViewById<FloatingActionButton>(R.id.add_notebook)
-        buttton.setOnClickListener{
+        val button = view.findViewById<FloatingActionButton>(R.id.add_notebook)
+        button.setOnClickListener{
             navController.navigate(R.id.action_mainFragment2_to_addFragment2)
         }
-    }
 
+        val recycler=view.findViewById<RecyclerView>(R.id.recycler)
+
+        context?.let { viewModels.getAllNotebooks(it) }
+        viewModels.list.observe(viewLifecycleOwner) {
+            adapter.setContentList(it)
+            recycler.also { recycler ->
+
+                recycler.adapter = adapter
+            }
+        }
+
+    }
 
 }
